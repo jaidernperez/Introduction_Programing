daf7a18eb24ba4ad5c2f01b2df66827ef871b045
jaiderperez <jaidernico408@gmail.com>
2020-04-16 11:29:09 -0500

    Improve project P1T6
    Add documentation

--- P2T6_Methods with time/src/usta/sistemas/Main.java
+++ P2T6_Methods with time/src/usta/sistemas/Main.java
@@ -2,12 +2,15 @@ package usta.sistemas;
 
 import java.util.Scanner;
 
+/**
+ * This software realize some time unit conversions
+ * @author  Jaider Pérez
+ * @version 15/04/2020
+ */
 public class Main {
-    /*
-        Author: Jaider Pérez
-        Date:15/04/2020
-        Description: This software realize some time unit conversions
-    */
+
+    private static final Scanner keyboard = new Scanner(System.in);
+
     public static void main(String[] args) {
         menu();
         int option = option();
@@ -29,9 +32,11 @@ public class Main {
         System.out.println("╚════════════════════════════════════════════════╝");
     }
 
+    /**
+     * This function return the option to convert
+     * @return Integer representing the option
+     */
     public static int option() {
-        // Description: This function return the option to convert
-        Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter the option of the conversion: ");
         System.out.println(" 1. Hour to second \n 2. Minutes to seconds \n 3. Seconds to hours and minutes");
         int option;
@@ -45,27 +50,30 @@ public class Main {
         return option;
     }
 
+    /**
+     * This function convert hours to seconds
+     */
     public static void hours_to_seconds() {
-        // Description: This function convert hours to seconds
-        Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter the number of hours to convert: ");
         int hours = keyboard.nextInt();
         int seconds = hours * 3600;
         System.out.println(hours + " hours equals to " + seconds + " seconds");
     }
 
+    /**
+     * This function convert minutes to seconds
+     */
     public static void minutes_to_seconds() {
-        // Description: This function convert minutes to seconds
-        Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter the numbers of minutes to convert: ");
         int minutes = keyboard.nextInt();
         int seconds = minutes * 60;
         System.out.println(minutes + " minutes equals to " + seconds + " seconds");
     }
 
+    /**
+     * This function convert seconds to hours and minutes
+     */
     public static void seconds_to_hours_minutes() {
-        // Description: This function convert seconds to hours and minutes
-        Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter the numbers of seconds to convert: ");
         int seconds = keyboard.nextInt();
         int hours = seconds / 3600;
